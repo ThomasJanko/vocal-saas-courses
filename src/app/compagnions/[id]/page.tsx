ca2d924122b057fcf58e4ae0ed7083b579feab13
Thomas Janko <thomas78125@gmail.com>
2025-06-11 15:43:42 +0200

    first commit

---
+++ src/app/compagnions/[id]/page.tsx
@@ -0,0 +1,7 @@
+const CompanionSession = ({ params }: { params: { id: string } }) => {
+  return (
+    <div>CompanionSession {params.id}</div>
+  )
+}
+
+export default CompanionSession
---
+++ src/app/compagnions/page.tsx
@@ -0,0 +1,9 @@
+const CompagnionsLibrary = () => {
+  return (
+    <div>
+      <h1>Compagnions Library</h1>
+    </div>
+  )
+}
+
+export default CompagnionsLibrary
